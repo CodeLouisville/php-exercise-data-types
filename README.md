e90e0f48f0d88b5d37278cb9575e5d88d23495ee
Jason McCreary <jason@pureconcepts.net>
2016-01-06 18:56:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Data Types in PHP
+
+For this PHP code exercise, create a file named `data_types.php`. This file should contain the following functions:
+ 
+- `convert_to_int()`
+- `convert_to_float()`
+- `convert_to_string()`
+- `convert_to_bool()`
+- `convert_to_array()`
+- `convert_to_null()`
+
+Each of these functions should accept one parameter as *input*. The function should *convert* the *input* into the respective data type and `return` the converted value. For example, `convert_to_int()` should return the *input* converted to the `int` data type, and `convert_to_string()` should return the *input* converted to the `string` data type, and so on.
+ 
+If a value can not be converted you should return the following *falsy* value for each data type:
+
+- Return `0` for `convert_to_int()`
+- Return `0.0` for `convert_to_float()`
+- Return an empty string for `convert_to_string()`
+- Return `false` for `convert_to_bool()`
+- Return `[]` for `convert_to_array()`
+- Return `null` for `convert_to_null()`
+
+There are a few special cases:
+
+1. If an `array` is passed as *input* to `convert_to_string()`, it should a string representation of the elements, in order, as a comma separated list. For example, an input of `[1, 2, 3]` should return `1, 2, 3`, an input of `[1]` should return `1`.
+2. If any of the *falsy* values above or the string `null` is passed as *input* to `convert_to_null()`, it should return `null`.
+
+## Just getting started?
+Review the [PHP Code Exercises](https://github.com/CodeLouisville/back-end-php/tree/master/exercises) documentation for more details on performing code exercises.
+
+## Need help?
+Jump on the PHP channel in Slack and ask your fellow students and mentors for a hint.
---
+++ tests/DataTypesTest.php
@@ -0,0 +1,149 @@
+<?php
+require 'data_types.php';
+
+class ArrayTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider boolTestData
+     */
+    public function testConvertToBool($value, $expected)
+    {
+        $this->assertSame($expected, convert_to_bool($value));
+    }
+
+    public function boolTestData()
+    {
+        return [
+            [0, false],
+            [0.0, false],
+            ['', false],
+            ['0', false],
+            [[], false],
+            [null, false],
+            [false, false],
+            [1, true],
+            [-1, true],
+            ['false', true],
+            [0.1, true],
+            [53, true],
+            ['true', true],
+            [[2, 3], true],
+            [true, true]
+        ];
+    }
+
+    /**
+     * @dataProvider intTestData
+     */
+    public function testConvertToInt($value, $expected)
+    {
+        $this->assertSame($expected, convert_to_int($value));
+    }
+
+    public function intTestData()
+    {
+        return [
+            [1, 1],
+            [53, 53],
+            ['7', 7],
+            ['3times', 3],
+            [0.3, 0],
+            [3.6, 3],
+            ['nope', 0],
+            ['', 0],
+            [[], 0],
+            [[2, 3], 1]
+        ];
+    }
+
+    /**
+     * @dataProvider floatTestData
+     */
+    public function testConvertToFloat($value, $expected)
+    {
+        $this->assertSame($expected, convert_to_float($value));
+    }
+
+    public function floatTestData()
+    {
+        return [
+            [1, 1.0],
+            ['1', 1.0],
+            ['7.2', 7.2],
+            ['3times', 3.0],
+            ['nope', 0.0],
+            ['', 0.0],
+            [[], 0.0],
+            [[2, 3], 1.0]
+        ];
+    }
+
+    /**
+     * @dataProvider stringTestData
+     */
+    public function testConvertToString($value, $expected)
+    {
+        $this->assertSame($expected, convert_to_string($value));
+    }
+
+    public function stringTestData()
+    {
+        return [
+            [1, '1'],
+            [5.3, '5.3'],
+            ['string', 'string'],
+            ['3times', '3times'],
+            ['', ''],
+            [[], ''],
+            [[2, 3], '2, 3'],
+            ['null', 'null'],
+            [null, '']
+        ];
+    }
+
+    /**
+     * @dataProvider arrayTestData
+     */
+    public function testConvertToArray($value, $expected)
+    {
+        $this->assertSame($expected, convert_to_array($value));
+    }
+
+    public function arrayTestData()
+    {
+        return [
+            [null, []],
+            [0, [0]],
+            ['7', ['7']],
+            ['value', ['value']],
+            [5.2, [5.2]],
+            ['', ['']],
+            [[], []],
+            [[2, 3], [2, 3]]
+        ];
+    }
+
+    /**
+     * @dataProvider nullTestData
+     */
+    public function testConvertToNull($value, $expected)
+    {
+        $this->assertSame($expected, convert_to_null($value));
+    }
+
+    public function nullTestData()
+    {
+        return [
+            [null, null],
+            ['', null],
+            ['0', null],
+            [0.0, null],
+            [false, null],
+            ['null', null],
+            [1, 1],
+            [true, true],
+            [[2, 3], [2, 3]],
+            [0.3, 0.3]
+        ];
+    }
+}
