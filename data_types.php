bc1fb61d06fa49070b3c43e67edf89edc260c470
Calvin Miracle <cbmira01@gmail.com>
2016-01-14 19:38:07 -0500

    Create data_types.php
    added case for array conversion to string

---
+++ data_types.php
@@ -0,0 +1,32 @@
+
+<?php
+
+    function convert_to_int($input) {
+			return intval($input);
+		};
+		
+    function convert_to_float($input) {
+			return floatval($input);
+		};
+		
+    function convert_to_string($input) {
+			if (is_array($input)) {
+				implode(",", $input);
+			} else {
+				return strval($input);
+			}
+		};
+		
+    function convert_to_bool($input) {
+			return boolval($input);
+		};
+		
+    function convert_to_array($input) {
+			return str_split($input);
+		};
+		
+    function convert_to_null($input)  {
+			return intval($input);
+		};
+
+?>		
