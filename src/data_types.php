db11709162e2b8e9eeb48fa00e7d096de4afd6e2
jmosley5 <jmosley87@gmail.com>
2016-01-19 13:48:06 -0500

    Create data_types.php
    convert_to_int()

---
+++ src/data_types.php
@@ -0,0 +1,20 @@
+<?php
+//covert_to_int:
+function convert_to_int($input = "0")
+{
+  return $input;
+}
+
+echo convert_to_int();
+
+
+//convert_to_float:
+
+
+convert_to_float()
+convert_to_string()
+convert_to_bool()
+convert_to_array()
+convert_to_null()
+
+?>
