3ad74dfc2a4f393136031b20da3ed248ea0699ef
Tony T <TonyTatsumaki@live.com>
2016-05-17 18:46:33 -0400

    First Attempt
    This will be bad.

---
+++ data_types.php
@@ -0,0 +1,31 @@
+﻿<?php
+
+function convert_to_int($input) {
+	intval($input);
+	return $input;
+}
+
+function convert_to_float($input) {
+	floatval($input);
+	return $input;
+}
+
+function convert_to_string($input) {
+	strval($input);
+	return $input;
+}
+
+function convert_to_bool($input) {
+	boolval($input);
+	return $input;
+}
+
+function convert_to_array($input) {
+	array([$input]);
+	return $input;	
+}
+
+function convert_to_null($input) {
+	$input = null;
+	return $input;
+}
