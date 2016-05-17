82b413d7cbb1c1d27850fc5723a099bac6f7eb3c
Matt English <menglish78@bellsouth.net>
2016-05-17 17:58:47 -0400

    First try...Here's goes nothin'!

---
+++ src/data_types.php
@@ -0,0 +1,18 @@
+$var = "Hello there, everybody!"
+
+convert_to_int($var)
+
+
+convert_to_float($var)
+
+
+convert_to_string($var)
+
+
+convert_to_bool($var)
+
+
+convert_to_array($var)
+
+
+convert_to_null($var)
