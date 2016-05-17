f5ea371c04fdd8bbcd4b75908881a4e89095e852
Matt English <menglish78@bellsouth.net>
2016-05-17 18:02:51 -0400

    Second try...

--- src/data_types.php
+++ src/data_types.php
@@ -1,18 +1,18 @@
-$var = "Hello there, everybody!"
+$var = "Hello there, everybody!";
 
-convert_to_int($var)
-
-
-convert_to_float($var)
+convert_to_int($var);
 
 
-convert_to_string($var)
+convert_to_float($var);
 
 
-convert_to_bool($var)
+convert_to_string($var);
 
 
-convert_to_array($var)
+convert_to_bool($var);
 
 
-convert_to_null($var)
+convert_to_array($var);
+
+
+convert_to_null($var);
