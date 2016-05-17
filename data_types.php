e4825d5e7ae26a15d4fdec851dcf819f58687435
Tony T <TonyTatsumaki@live.com>
2016-05-17 19:11:06 -0400

    Third shot!
    Okay, might not be so bad.

--- data_types.php
+++ data_types.php
@@ -1,34 +1,30 @@
 ﻿<?php
 
 function convert_to_int($input) {
-	intval($input);
-	return $input;
+	return is_int((int)$input)?(int)$input:0; 
 }
 
 function convert_to_float($input) {
-	floatval($input);
-	return $input;
+	return is_float((float)$input)?(float)$input:0.0;
 }
 
 function convert_to_string($input) {
-	strval($input);
-	return $input;
+	if (is_array($input))
+    return implode(', ',$input);
+   	return is_string((string)$input)?(string)$input:'';
 }
 
 function convert_to_bool($input) {
-	boolval($input);
-	return $input;
+	return is_bool((bool)$input)?(bool)$input:false;
 }
 
 function convert_to_array($input) {
-	implode([$input]);
-	$input = array([$input]);
-	return $input;
+	return is_array((array)$input)?(array)$input:[];
 }
 
 function convert_to_null($input) {
-	$input = null;
-	return $input;
+	if ($input==='null') $input=null;
+    return convert_to_bool($input)?$input:null;
 }
 
 ?>
