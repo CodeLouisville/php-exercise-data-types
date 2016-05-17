c5d0cf87b205aedac44ae77175c26be70d1e119b
jgiese328 <jgiese328@gmail.com>
2016-05-17 18:54:34 -0400

    Add files via upload
    Difficult to follow instructions, think I got it for this challenge though.

---
+++ src/data_types.php
@@ -0,0 +1,38 @@
+<?php
+
+// ultimately you would know what each of these functions would do in an actual development environment. 
+
+function convert_to_int($value) {
+	if(empty($value))
+	{
+		$value = 0;
+	}
+	return intval($value);
+}
+function convert_to_float($value) {
+	if(empty($value)) {
+		$value = 0.0;
+	}
+	return floatval($value);	
+}
+function convert_to_string($value) {
+	if(is_array($value))
+		$value = implode(',',$value);
+	 return strval($value);
+}
+function convert_to_bool($value) {
+	return boolval($value);
+}
+function convert_to_array($array) {
+	$result = explode(',',$array);
+	
+	if(is_array($result))
+		return $result;
+	else 
+		return array();
+}
+function convert_to_null($value) {
+	return NULL;
+}
+
+?>
