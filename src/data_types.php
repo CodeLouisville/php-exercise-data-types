3ac40fc37990c93df6685075a04bac67a2fe1140
micahpotts <mrs.micahpotts@gmail.com>
2016-05-24 18:20:19 -0400

    Added empty functions for each
    Trying to avoid fatal error. Seeing if I can check each problem singly
    as I go.

--- src/data_types.php
+++ src/data_types.php
@@ -3,10 +3,11 @@
 // convert_to_int()
 
 	function convert_to_int($int){
+		
 		return intval($int);
 	}
 
-	echo convert_to_int('666');
+	//echo convert_to_int('666');
 	//echo convert_to_int('dog');
 
 
@@ -22,21 +23,44 @@
 	}
 
 
-	$float = convert_to_float('fuuuk');
+	//$float = convert_to_float('fuuuk');
 	//$float = convert_to_float('3.14159PIETOWNANDZBOYS');
 	
-	var_dump($float);
-	echo $float;
+	//var_dump($float);
+	
+	//echo $float;
 
 
 //convert_to_string()
 
-	//function convert_to_string($num){
-	//	return strval($num);
-	//}
+	function convert_to_string($num){
+		
+		if(strval($num) != FALSE){
+			return strval($num);
+		} else {
+			return "";
+			}
+	}
 
+	//$num = convert_to_string(FALSE);
 	//$num = convert_to_string(8008);
-
+	
 	//echo gettype($num);
 
 	//echo $num;
+
+
+//convert_to_bool
+
+	function convert_to_bool() {}
+
+
+//convert_to_array()
+
+	function convert_to_array() {}
+
+
+//convert_to_null()
+
+	function convert_to_null() {}
+
