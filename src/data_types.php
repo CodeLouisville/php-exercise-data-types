e2486eb054dcfabe0cdf5eecb79284a010712278
micahpotts <mrs.micahpotts@gmail.com>
2016-05-25 11:27:32 -0400

    Up to convert_to_array - to check answers so far

--- src/data_types.php
+++ src/data_types.php
@@ -7,8 +7,9 @@
 		return intval($int);
 	}
 
-	//echo convert_to_int('666');
+	$num = convert_to_int('666');
 	//echo convert_to_int('dog');
+	var_dump($num);
 
 
 // convert_to_float()
@@ -22,42 +23,61 @@
 		}
 	}
 
-
-	//$float = convert_to_float('fuuuk');
+	$float = convert_to_float('foo');
 	//$float = convert_to_float('3.14159PIETOWNANDZBOYS');
 	
-	//var_dump($float);
-	
+	var_dump($float);
 	//echo $float;
 
 
 //convert_to_string()
 
-	function convert_to_string($num){
-		
-		if(strval($num) != FALSE){
-			return strval($num);
+	function convert_to_string($str){
+		if(is_array($str)){
+			return implode("," , $str);
+		} else if(settype($str, 'string')) {
+			return $str;
 		} else {
 			return "";
-			}
+		}
 	}
 
-	//$num = convert_to_string(FALSE);
-	//$num = convert_to_string(8008);
-	
-	//echo gettype($num);
+	$var = convert_to_string(55);
+	//$var = convert_to_string(array('foo', 'bar'));
 
-	//echo $num;
+	//echo $var;
+	var_dump($var);
+
+
 
 
 //convert_to_bool
 
-	function convert_to_bool() {}
+	function convert_to_bool($boo) {
+		return (bool) $boo;
+	}
+	
+	$bool = convert_to_bool(null);
+	var_dump($bool);
 
 
 //convert_to_array()
 
-	function convert_to_array() {}
+	function convert_to_array($arrgh) {
+		if (is_string($arrgh) || is_bool($arrgh) || is_float($arrgh)) {
+		return str_split($arrgh);
+	} elseif (is_int($arrgh)) {
+		return array_map('intval', str_split($arrgh));
+	} elseif (str_split($arrgh)) {
+		return str_split($arrgh);
+	} else {
+		return [];
+		}
+	}
+	
+
+	$yayarray = convert_to_array(null);
+	var_dump($yayarray);
 
 
 //convert_to_null()
