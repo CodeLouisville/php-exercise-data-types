84bf4fff3245e1bdee5c8364599724fd3d1e56a3
Nicholas Dixon <nldixon@gmail.com>
2016-05-31 23:08:22 +0000

    PHP data types take 2

--- src/data_types.php
+++ src/data_types.php
@@ -3,55 +3,49 @@
 function convert_to_int($input) {
     if (settype($input, "int")) {
         return $input;
-        var_dump($input);
     } else {
         return 0;
-        var_dump($input);
+    }
 }
 
 function convert_to_float($input) {
     if (settype($input, "float")) {
         return $input;
-        var_dump($input);
     } else {
         return 0.0;
-        var_dump($input);
+    }
 }
 
 function convert_to_string($input) {
     if (settype($input, "string")) {
         return $input;
-        var_dump($input);
     } else {
         return "";
-        var_dump($input);
+    }
 }
 
 function convert_to_bool($input) {
     if (settype($input, "bool")) {
         return $input;
-        var_dump($input);
     } else {
         return false;
-        var_dump($input);
+    }
 }
 
 function convert_to_array($input) {
     if (settype($input, "array")) {
         return $input;
-        var_dump($input);
     } else {
         return [];
-        var_dump($input);
+    }
 }
 
 function convert_to_null($input) {
     if (settype($input, "null")) {
         return $input;
-        var_dump($input);
     } else {
         return null;
-        var_dump($input);
+    }
 }
 
 ?>
