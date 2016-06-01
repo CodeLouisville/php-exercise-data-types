a77e7f90ef901b2a648df24c5817c7b82af627a9
John Nall <nall.john@twc.com>
2016-06-01 11:48:28 -0400

    Add files via upload

---
+++ data_types.php
@@ -0,0 +1,60 @@
+<?php
+
+function convert_to_int($input) {
+  if($input > 0) {
+    $conval = intval($input);
+  } else {
+    $conval = 0;
+}
+  return $conval;
+}
+
+function convert_to_float($input) {
+  if($input > 0) {
+    $conval = floatval($input);
+  } else {
+  $conval = 0.0;
+}
+  return $conval;
+  }
+
+function convert_to_string($input) {
+  if(is_array($input)) {
+    $conval = implode(", ", $input);
+  } else if($input) {
+    $conval = strval($input);
+} else {
+  $conval = $input;
+}
+  return $conval;
+}
+
+function convert_to_bool($input = null) {
+  if($input) {
+    $conval = boolval($input);
+  } else {
+    $conval = false;
+}
+  return $conval;
+}
+
+function convert_to_array($input) {
+  if(is_array($input)) {
+    $conval = $input;
+  } else if($conval = null) {
+    $conval = null;
+  } else {
+    $conval = 0;
+  }
+  return $conval;
+}
+
+  function convert_to_null($input){
+    if(is_null($input)) {
+      $conval = null;
+    } else {
+      $conval = $input;
+    }
+    return $conval;
+  }
+  ?>
