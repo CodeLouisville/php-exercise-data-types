260f66fd01086402fe5ee8941c7cb78e07a73a6d
GregoryC55 <gregory.cunningham8276@gmail.com>
2016-06-07 18:38:14 -0400

    13

--- src/data_types.php
+++ src/data_types.php
@@ -34,15 +34,25 @@ function convert_to_bool($value) {
   }
 }
 
-function convert_to_array($value) {
+//function convert_to_array($value) {
+//  if (!$value) {
+//    return [];
+//  } else if (is_array($value)) {
+//    return $value;
+//  } else {
+//    return [$value];
+//  }
+//}
+//var_dump (convert_to_array(''));
+
+function convert_to_array ($value) {
   if (!$value) {
     return [];
-  } else if (is_array($value)) {
-    return $value;
-  } else {
-    return [$value];
-  }
+} else {
+  return (array)$value;
 }
+
+//var_dump (convert_to_array(''));
 
 function convert_to_null($value) {
   if (!$value || $value === "null") {
@@ -51,7 +61,5 @@ function convert_to_null($value) {
     return $value;
   }
 }
-
-
 //var_dump (convert_to_int(4.2), convert_to_float(4.2), convert_to_string([4, 5, 6]), convert_to_bool(4.2), convert_to_array(4.2), convert_to_null(4.2));
 ?>
