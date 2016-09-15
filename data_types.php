c5e67cdaa761687afb643c2ac2b9416351f5187e
Schuyler Grisham <Schuyler Grisham>
2016-09-15 16:25:20 -0400

    second commit for challenge

--- data_types.php
+++ data_types.php
@@ -2,42 +2,45 @@
 <?php
 //convert_to_int()
 function convert_to_int($input){
-if (is_string($input)) { $input = 0;
-}
   return intval($input);
 }
 //convert_to_float()
 function convert_to_float($input){
-if (is_string($input)) { $input = 0.0;
-}
   return floatval($input);
 }
 //convert_to_string()
 function convert_to_string($input){
-if (is_array($input)) { return implode(",", $input);
-}
-  return (string)$input;
+if (is_array($input)) { $input = implode(",", $input);
+  } else {
+    $input = strval($input);
+  }
+  return $input;
 }
 //convert_to_bool()
 function convert_to_bool($input){
-  if (!$input) {
+  if ($input) {
+    return true;
+  } else {
     return false;
   }
-  return settype($input,'bool');
 }
 //convert_to_array()
 function convert_to_array($input){
-  if ($input == null) { return [];
-}
-  return str_split($input);
+  if ($input === null) {
+    return array();
+  } else if (is_array($input)) {
+    return $input;
+  } else {
+    return array($input);
+  }
 }
 //convert_to_null()
 function convert_to_null($input){
-  if (!$input || $input == "null"){
+  if (!$input || $input === "null"){
     return null;
   } else {
     return $input;
   }
 }
 
- ?>
+?>
